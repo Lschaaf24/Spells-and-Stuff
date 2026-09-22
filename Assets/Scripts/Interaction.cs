10cce877bae8d9dc46cc252f7a27f3a6a591314d
Liam Schaaf <Liam2.Schaaf@live.uwe.ac.uk>
2026-09-22 14:23:18 +0100

    Fixed outline

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -93,7 +93,7 @@ public class Interaction : MonoBehaviour
         if (objectRenderer == null) return;
 
         Material[] materialArray = new Material[objectRenderer.materials.Length - 1];
-        for (int i = 0; i < materialArray.Length - 1; i++)
+        for (int i = 0; i < materialArray.Length; i++)
         {
             materialArray[i] = objectRenderer.materials[i];
         }
